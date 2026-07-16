538eec1a83093e1dcfda75e8092708036190b6c3
S1mpleWind <ivonjiao@gmail.com>
2026-07-16 21:33:55 +0800

    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://10.119.4.70:8000'),
+    // 服务器后端
+    //'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://10.119.4.70:8000'),
+
     // 本地调试用：
-    // 'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://localhost:8000'),
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://localhost:8000'),
   },
 })
